83f5d0284c883bac8bf392decc30b3ec3d6b9538
Kieran Andrews <kieran.andrews@adelaide.edu.au>
2017-04-19 12:36:32 +0930

    Initial Commit

---
+++ ltdvideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ltdvideo
+//
+//  Created by Kieran Andrews on 19/4/17.
+//  Copyright © 2017 Kieran Andrews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
